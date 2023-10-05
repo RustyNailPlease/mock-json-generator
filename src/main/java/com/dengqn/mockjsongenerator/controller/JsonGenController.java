3294f1eb0a8d10ff7e785453f3890cdfd8496030
dengqn <434500374@qq.com>
2023-10-05 11:51:56 +0800

    DEL

--- src/main/java/com/dengqn/mockjsongenerator/controller/JsonGenController.java
+++ src/main/java/com/dengqn/mockjsongenerator/controller/JsonGenController.java
@@ -1,34 +1,65 @@
 package com.dengqn.mockjsongenerator.controller;
 
 
-import cn.hutool.json.JSON;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
 import com.dengqn.mockjsongenerator.service.JsonGenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/api/json")
 public class JsonGenController {
 
-    @Autowired
-    private JsonGenService jsonGenService;
+	@Value("${template.dir:./template}")
+	private String templateDir;
 
 
-    /**
-     * 生成
-     *
-     * @param jsonString
-     * @return
-     */
-    @PostMapping("/gen")
-    public ResponseEntity<Object> gen(@RequestBody String json) {
-        return ResponseEntity.ok().body(jsonGenService.genJson(JSONUtil.parse(json)));
-    }
+	@Autowired
+	private JsonGenService jsonGenService;
 
+
+	/**
+	 * 生成
+	 *
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/gen/{id}")
+	public ResponseEntity<Object> gen(@PathVariable("id") String id) {
+		return ResponseEntity.ok().body(jsonGenService
+				.genJson(JSONUtil.parse(FileUtil.readString(templateDir + File.separator + id + ".template", StandardCharsets.UTF_8))));
+	}
+
+	/**
+	 * 添加模板
+	 *
+	 * @param template
+	 * @return
+	 */
+	@PostMapping("/template")
+	public ResponseEntity<String> addTemplate(@RequestBody String template) {
+		String string = RandomUtil.randomString(20);
+
+		try {
+			gen(template);
+		} catch (Exception e) {
+			return ResponseEntity.internalServerError().body(e.getMessage());
+		}
+
+
+		FileUtil.writeBytes(template.getBytes(), templateDir + File.separator + string + ".template");
+
+		return ResponseEntity.ok().body(string);
+
+
+	}
 
 }
